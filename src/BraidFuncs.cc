bf7ab315b4a2d5c6ba11ba263467aef58bc53c55
jcc242 <chrisjoshtopher@gmail.com>
2018-04-12 08:54:40 -0600

    Added UNUSED macro for unused function parameters

--- src/BraidFuncs.cc
+++ src/BraidFuncs.cc
@@ -7,6 +7,8 @@ int my_Step(braid_App        app,
             braid_Vector     u,
             braid_StepStatus status)
 {
+  UNUSED(ustop);
+  UNUSED(fstop);
   double tstart;             /* current time */
   double tstop;              /* evolve to this time*/
   int level;
@@ -49,6 +51,7 @@ my_Clone(braid_App     app,
          braid_Vector  u,
          braid_Vector *v_ptr)
 {
+  UNUSED(app);
   my_Vector *v = new(my_Vector);
   int size = u->data.size();
   v->data.reinit(size);
@@ -65,6 +68,7 @@ int
 my_Free(braid_App    app,
         braid_Vector u)
 {
+  UNUSED(app);
   delete u;
 
   return 0;
@@ -76,6 +80,7 @@ int my_Sum(braid_App app,
            double beta,
            braid_Vector y)
 {
+  UNUSED(app);
   Vector<double>& vec = y->data;
   vec.sadd(beta, alpha, x->data);
 
@@ -87,6 +92,7 @@ my_SpatialNorm(braid_App     app,
                braid_Vector  u,
                double       *norm_ptr)
 {
+  UNUSED(app);
   double dot = 0.0;
   dot = u->data.l2_norm();
   *norm_ptr = dot;
@@ -99,6 +105,9 @@ my_Access(braid_App          app,
           braid_Vector       u,
           braid_AccessStatus astatus)
 {
+  UNUSED(app);
+  UNUSED(u);
+  UNUSED(astatus);
   // int        index, rank, level, done;
   // double     t;
   // int index;
@@ -125,6 +134,7 @@ my_BufSize(braid_App           app,
            int                 *size_ptr,
            braid_BufferStatus  bstatus)
 {
+  UNUSED(bstatus);
   int size = app->eq.size();
   *size_ptr = (size+1)*sizeof(double);
 
@@ -137,6 +147,8 @@ my_BufPack(braid_App           app,
            void               *buffer,
            braid_BufferStatus  bstatus)
 {
+
+  UNUSED(app);
   double *dbuffer = (double*)buffer;
   int size = u->data.size();
   dbuffer[0] = size;
@@ -155,6 +167,9 @@ my_BufUnpack(braid_App           app,
              braid_Vector       *u_ptr,
              braid_BufferStatus  bstatus)
 {
+  UNUSED(app);
+  UNUSED(bstatus);
+
   my_Vector *u = NULL;
   double *dbuffer = (double*)buffer;
   int size = dbuffer[0];
